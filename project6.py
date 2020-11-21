c22cc393aca424fe880933fad2f028c0ebc489b6
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-11-21 18:11:51 -0500

    init

---
+++ project6.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def knn():
+    pass 
+
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
